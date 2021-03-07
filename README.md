40e0e1a289d96b5091e7ca45a54b80d8ba7ce124
root <root@100-064-164-082.(none)>
2021-03-07 19:44:22 +0000

    plenty of changes

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # r1soft_migrate
+## Purpose
+This script is designed to gather important information regarding the r1soft backup solution and disk configuration on the server, in preparateion for migration.
+## Use
+usual usage would be ./r1soft.py -p <password>
+for further options call ./r1soft.py --help
--- r1soft.py
+++ r1soft.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python
-import base64
-import urllib
-import urllib2
-import xml.etree.ElementTree as ET
-import os
+import base64, urllib2, xml.etree.ElementTree, os, sys, socket, tempfile, subprocess
+from optparse import OptionParser
 
-username=os.environ.get('username')
-password=os.environ.get('password')
-base_url='http://localhost:9080/'
+def log_shell_cmd(cmd, heading):
+    p = subprocess.Popen(cmd, shell=True,stdout=subprocess.PIPE)
+    (output, err) = p.communicate()
+    p.wait()
+    writer(heading)
+    writer(output)
 
-auth_key = base64.b64encode('%s:%s' % (username, password))
-headers = {'Authorization': 'Basic %s' % auth_key}
+def writer(string):
+    print string
+    with open('{0}/r1soft_report'.format(data_dir),'a') as report:
+        report.write(string)
+        report.write('\n')
+
+def test_port():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    result = sock.connect_ex((api_host,api_port))
+    sock.close()
+    if result != 0:
+        print "API Service is no listening on port {0}".format(api_port)
+        sys.exit()
+
+def set_header(username,password):
+    global headers
+    auth_key = base64.b64encode('%s:%s' % (username, password))
+    headers = {'Authorization': 'Basic %s' % auth_key}
 
 def api_request(url_end_part,data,headers):
    
-    full_url='%s%s' % (base_url, url_end_part)
+    full_url='%s%s' % ('http://{0}:{1}/'.format(api_host,api_port), url_end_part)
     req = urllib2.Request(full_url,data,headers=headers)
     response = urllib2.urlopen(req)
     the_page = response.read()
@@ -40,37 +56,26 @@ def print_data(data,title):
                     fieldlen[key] = (len(val) + 2)
         for key,val in fieldlen.iteritems():
             fstr += '{%s:<%s}' % (key,val)
-        print '\n{0} :\n'.format(title)
+        writer('\n{0} CONFIGURATION:\n'.format(title).upper())
         for i in data:
-            print fstr.format(**i)
+            writer(fstr.format(**i))
 
-
-
-def perform_volumes_tasks():
-
-    data='''<Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
-        <Body>
-            <getVolumes xmlns="http://volume.api.server.backup.r1soft.com/"/>
-        </Body>
-        </Envelope>
-         '''
-    x = api_request('Volume?wsdl',data,headers)
-    with open('output_volumes.xml','w') as outfile:
+def analyse(search):
+    x = api_request(search['endpoint'],search['soap'],headers)
+    with open('{1}/output_{0}.xml'.format(search['space'],data_dir), 'w') as outfile:
         outfile.write(x)
-    vd = get_volumes(x)
-    vd = add_mount_point(vd)
-    print_data(vd,'VOLUME CONFIGURATION')
-
-def get_volumes(response_xml):
-    xmlroot = ET.fromstring(response_xml)
-    volumes =  xmlroot.findall('{http://schemas.xmlsoap.org/soap/envelope/}Body/{http://volume.api.server.backup.r1soft.com/}getVolumesResponse/return')
-    volumes_data=[]
-    for i in volumes:
-        volume = {}
-        volume['name'] = i.find('name').text
-        volume['path'] = i.find('path').text
-        volumes_data.append(volume)
-    return volumes_data
+    xmlroot = xml.etree.ElementTree.fromstring(x)
+    sd = xmlroot.findall(search['search_path'])
+    data_list = []
+    for i in sd:
+        data = {}
+        for x in search['items']:
+            data[x] = i.find(x).text
+        data_list.append(data)
+    if search['p_processors']:
+        for i in search['p_processors']:
+            data_list = i(data_list)
+    return data_list
 
 def add_mount_point(volumes):
     for i in volumes:
@@ -83,61 +88,99 @@ def find_mount_point(path):
         path = os.path.dirname(path)
     return path
 
-
-def perform_disk_safe_tasks():
-    data='''
-        <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
-    <Body>
-        <getDiskSafes xmlns="http://disksafe.api.server.backup.r1soft.com/"/>
-    </Body>
-</Envelope>
-         '''
-    x = api_request('DiskSafe?wsdl',data,headers)
-    with open('output_disksafe.xml', 'w') as outfile:
-        outfile.write(x)
-    dsd = get_disk_safes(x)
-    print_data(dsd,'DISK SAFE CONFIGURATION')
-
-def get_disk_safes(response_xml):
-    xmlroot = ET.fromstring(response_xml)
-    disksafes = xmlroot.findall('{http://schemas.xmlsoap.org/soap/envelope/}Body/{http://disksafe.api.server.backup.r1soft.com/}getDiskSafesResponse/return')
-    disk_dafe_data = []
-    for i in disksafes:
-        ds = {}
-        ds['path'] = i.find('path').text
-        ds['description'] = i.find('description').text
-        ds['size'] = i.find('size').text
-        ds['recovery points'] = i.find('recoveryPointCount').text
-        disk_dafe_data.append(ds)
-    return disk_dafe_data
-
-def perform_user_tasks():
-    data='''
-        <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
-        <Body>
-            <getUsers xmlns="http://user.api.server.backup.r1soft.com/"/>
-        </Body>
-        </Envelope>
-        '''
-    x = api_request('User?wsdl',data,headers)
-    with open('output_user.xml', 'w') as outfile:
-        outfile.write(x)
-    ud = get_users(x) 
-    print_data(ud,'USER CONFIRUGRATION')
-
-def get_users(response_xml):
-    xmlroot = ET.fromstring(response_xml)
-    users = xmlroot.findall('{http://schemas.xmlsoap.org/soap/envelope/}Body/{http://user.api.server.backup.r1soft.com/}getUsersResponse/return')
-    user_data = []
-    for i in users:
-        user = {}
-        user['username'] = i.find('username').text
-        user['userType'] = i.find('userType').text
-        user_data.append(user)
-    return user_data
-
+search_values = [
+                {'soap':'''
+                        <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+                        <Body>
+                        <getVolumes xmlns="http://volume.api.server.backup.r1soft.com/"/>
+                        </Body>
+                        </Envelope>
+                        ''',
+                'endpoint': 'Volume?wsdl',
+                'search_path': '{http://schemas.xmlsoap.org/soap/envelope/}Body/{http://volume.api.server.backup.r1soft.com/}getVolumesResponse/return',
+                'items': ['name', 'path'],
+                'space': 'volume',
+                'p_processors': [add_mount_point,],
+                },
+                {'soap':'''
+                        <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+                        <Body>
+                        <getDiskSafes xmlns="http://disksafe.api.server.backup.r1soft.com/"/>
+                        </Body>
+                        </Envelope>
+                        ''',
+                'endpoint': 'DiskSafe?wsdl',
+                'search_path': '{http://schemas.xmlsoap.org/soap/envelope/}Body/{http://disksafe.api.server.backup.r1soft.com/}getDiskSafesResponse/return',
+                'items': ['path', 'description', 'size', 'recoveryPointCount'],
+                'space': 'disksafe',
+                'p_processors': [],
+                },
+                {'soap':'''
+                        <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+                        <Body>
+                        <getUsers xmlns="http://user.api.server.backup.r1soft.com/"/>
+                        </Body>
+                        </Envelope>
+                        ''',
+                'endpoint': 'User?wsdl',
+                'search_path': '{http://schemas.xmlsoap.org/soap/envelope/}Body/{http://user.api.server.backup.r1soft.com/}getUsersResponse/return',
+                'items': ['username', 'userType'],
+                'space' : 'user',
+                'p_processors': [],
+                },
+                {'soap':'''
+                        <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+                        <Body>
+                        <getAgents xmlns="http://agent.api.server.backup.r1soft.com/"/>
+                        </Body>
+                        </Envelope>
+                        ''',
+                'endpoint': 'Agent?wsdl',
+                'search_path': '{http://schemas.xmlsoap.org/soap/envelope/}Body/{http://agent.api.server.backup.r1soft.com/}getAgentsResponse/return',
+                'items' : ['description', 'hostname', 'osType'],
+                'space' : 'agent',
+                'p_processors': [],
+                },
+                {'soap':'''
+                        <Envelope xmlns="http://schemas.xmlsoap.org/soap/envelope/">
+                        <Body>
+                        <getPolicies xmlns="http://policy.apiV2.server.backup.r1soft.com/"/>
+                        </Body>
+                        </Envelope>
+                        ''',
+                'endpoint': 'Policy2?wsdl',
+                'search_path': '{http://schemas.xmlsoap.org/soap/envelope/}Body/{http://policy.apiV2.server.backup.r1soft.com/}getPoliciesResponse/return',
+                'items' : ['description', 'name', 'enabled', 'replicationScheduleFrequencyType'],
+                'space' : 'policy',
+                'p_processors': [],
+                },
+                ]
 
 if __name__ == '__main__':
-    perform_volumes_tasks()
-    perform_disk_safe_tasks()
-    perform_user_tasks()
+    parser = OptionParser()
+    parser.add_option('-u', '--username', help='r1soft username, overrides default armoradmin', default='armoradmin')
+    parser.add_option('-p', '--password', help='r1soft password')
+    parser.add_option('-P', '--port', help='api port, default=9080', default='9080')
+    parser.add_option('-i', '--host', help='api host IP, default=127.0.0.1', default='localhost')
+    options, args = parser.parse_args() 
+    global api_port
+    global api_host
+    api_port=int(options.port)
+    api_host='127.0.0.1'
+    if not options.password:
+        print "\noption -p must be used with a valid password\n"
+        sys.exit()
+    set_header(options.username,options.password)
+    test_port()
+    global data_dir
+    data_dir = tempfile.mkdtemp()
+    try:
+        for i in search_values:
+            results = analyse(i)
+            print_data(results, i['space'])
+        log_shell_cmd( "mount | grep -Ev 'type proc|type sysfs|type devpts|type tmpfs| type binfmt_misc'", '\nCURRENT FILESYSTEM MOUNTS :\n')
+        log_shell_cmd("grep -iPv '^[^/|u]|^$' /etc/fstab", '\nCURRENT FSTAB ENTRIES\n')
+        log_shell_cmd("pvs;echo;vgs;echo;lvs", '\nCURRENT LVM CONFIGURATION\n')
+        print '\nReport written to {0}/r1soft_report\n'.format(data_dir)
+    except Exception as e:
+        print '\nApplication failed with the following error: %s\n' % e
